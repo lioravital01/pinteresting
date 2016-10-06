19fa087f41d01c6c8acfbff41bd637646d2391a1
Lior <lioravital01@gmail.com>
2016-10-06 00:43:43 -0400

    create home and about

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This is the Pinteresting sample app for
 [*one month rails*])http://onemonthrails.com)
 
-by [Lior Avital]
+by Lior Avital
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+root "pages#home"
+#makes homepage
+
+get "about" => "pages#about"
+#format for creating a new route for a page we made with a view and a controller
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
